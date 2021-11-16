a454c7702da1c0af4df37da6b40ccc1df8bea884
ahadriaz99 <67806421+ahadriaz99@users.noreply.github.com>
2021-11-16 14:32:58 +0000

    Configurations updated

--- config.py
+++ config.py
@@ -20,6 +20,29 @@ def configurations(N, M):
     valid_configs = list(map(tuple,valid_configs))
     return valid_configs
 
+def config_ang(N,M,L):
+    
+    config_input = np.array(configurations(N, M)) # Calculate repeated combinations
+    #print('configurations', config_input)
+        
+    index = 0
+    basis = []
+    for i in range(len(config_input)):
+        #print(i)
+        assert len(config_input[i]) == M # Check correct input format
+        vector = fock_vector(N, M, config_input[i])
+        # Only add restricted ang. mom. bases to the Fock spaces
+        if(vector.ang_mom() == L):
+            basis.append(fock_vector(N, M, config_input[i],index=index)) # Create fock vectors
+            index += 1
+          
+    
+    for basis in basis:
+        return(basis.print_info())
+    
+    
+    with open('Restricted_Bases_N%dM%d.txt'%(N, M), 'a') as f:
+            f.write('&N= %d, M= %d, L=%d, \n&END\n'%(self.M, self.N))
 #Test case
 #configurations(2,2)
 #configurations(3,2)
